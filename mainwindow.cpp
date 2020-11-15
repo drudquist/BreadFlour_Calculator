482658b421d7e9fd70591f67f37dee6f0c7fba04
Donald Rudquist <donaldrudquist@gmail.com>
2020-11-14 18:26:34 -0600

    validate number input, needs decimal point;  changed layout, grouped radio buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QRegExpValidator>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //allow only numbers
+    ui->bf_Amount->setValidator(new QRegExpValidator(QRegExp("[0-9]+,[0-9][0-9]*")));
+    ui->apf_Amount->setValidator(new QRegExpValidator(QRegExp("[0-9]+,[0-9][0-9]*")));
+    ui->vwg_Amount->setValidator(new QRegExpValidator(QRegExp("[0-9]+,[0-9][0-9]*")));
 }
 
 MainWindow::~MainWindow()
