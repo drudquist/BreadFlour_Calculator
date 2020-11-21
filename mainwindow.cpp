159f344a376d57ca6e29e3fa974a13c5e4ad07b7
Donald Rudquist <donaldrudquist@gmail.com>
2020-11-21 15:41:27 -0600

    Using formula to calculate, standardized units to grams, removed conversions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,16 @@
 
 #include <QRegExpValidator>
 
+/*Terminology
+    TPP = target protein percentage
+    FPP = flour protein percentage
+    VWGPP = vital wheat gluten protein percentage
+    apf_Percentage = percent of all purpose flour to use
+    vwg_Percentage = percent of vital wheat gluten to use
+
+    apf_Percentage = ( TPP - VWGPP * vwg_Percentage) / FPP
+    vwg_Percentage = ( 100 * FPP -  100 * TPP ) / ( FPP - VWGPP * ( 100 - FPP ) )
+*/
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,10 +20,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    const auto number_decimal_only = QRegExp("^\\d*(\\.\\d+)?$");
     //allow only numbers and decimal point
-    ui->bf_Amount->setValidator(new QRegExpValidator(QRegExp("^\\d*(\\.\\d+)?$")));
-    ui->apf_Amount->setValidator(new QRegExpValidator(QRegExp("^\\d*(\\.\\d+)?$")));
-    ui->vwg_Amount->setValidator(new QRegExpValidator(QRegExp("^\\d*(\\.\\d+)?$")));
+    ui->bf_Amount->setValidator(new QRegExpValidator(number_decimal_only));
+    ui->apf_Amount->setValidator(new QRegExpValidator(number_decimal_only));
+    ui->vwg_Amount->setValidator(new QRegExpValidator(number_decimal_only));
+    ui->bf_ProteinPct->setValidator(new QRegExpValidator(number_decimal_only));
+    ui->apf_ProteinPct->setValidator(new QRegExpValidator(number_decimal_only));
+    ui->vwg_ProteinPct->setValidator(new QRegExpValidator(number_decimal_only));
+
+    connect(ui->calculate, SIGNAL(clicked()), this, SLOT(Calculate()));
 }
 
 MainWindow::~MainWindow()
@@ -21,3 +37,58 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::Calculate()
+{
+     const QPushButton* const button = static_cast<const QPushButton*>(sender());
+
+     if(button != ui->calculate)
+     {
+         //application is broken
+         throw;
+     }
+
+    const double bf_Amount = ui->bf_Amount->text().toDouble();
+    const double tpp = ui->bf_ProteinPct->text().toDouble();
+    const double fpp = ui->apf_ProteinPct->text().toDouble();
+    const double vwgpp = ui->vwg_ProteinPct->text().toDouble();
+
+
+    if(bf_Amount <= 0.0 || tpp <= 0.0 || fpp <= 0.0 || vwgpp <= 0.0)
+    {
+        //TODO: need pop up window informing of needing these values to be greater than zero.
+        return;
+    }
+
+    //apf_ProteinPct cannot be greater than vwg_ProteinPct
+    if(fpp >= vwgpp)
+    {
+        //TODO: need pop up window informing of backwards values
+        return;
+    }
+
+    //Calculate Percentages
+    const double vwg_Percentage = CalculateVitalWheatGlutenPercentage(tpp, fpp, vwgpp);
+    const double apf_Percentage = CalculateAllPurposeFlourPercentage(vwg_Percentage, tpp, fpp, vwgpp);
+
+    //Calculate Amounts
+    const double vwg_Amount = bf_Amount * vwg_Percentage;
+    const double apf_Amount = bf_Amount * apf_Percentage;
+
+    //TODO: needs truncation
+    ui->vwg_Amount->setText(QString::number(vwg_Amount));
+    ui->apf_Amount->setText(QString::number(apf_Amount));
+}
+
+double MainWindow::CalculateVitalWheatGlutenPercentage(const double tpp, const double fpp, const double vwgpp) const
+{
+    //vwg_Percentage = ( 100 * FPP -  100 * TPP ) / ( FPP - VWGPP * ( 100 - FPP ) )
+
+    return ( 100 * fpp - 100 * tpp ) / ( fpp - vwgpp * ( 100 - fpp ) );
+}
+
+double MainWindow::CalculateAllPurposeFlourPercentage(const double vwg_Percentage, const double tpp, const double fpp, const double vwgpp) const
+{
+    //apf_Percentage = ( TPP - VWGPP * vwg_Percentage) / FPP
+
+    return ( tpp - vwgpp * vwg_Percentage ) / fpp;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+class QAbstractButton;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +17,30 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    enum class Units{
+        eCUPS,
+        eTBSP,
+        eTSP,
+        eGRAMS,
+        eOUNCES,
+    };
+
+    enum class Ingredient{
+        eBREAD_FLOUR,
+        eALL_PURPOSE_FLOUR,
+        eVITAL_WHEAT_GLUTEN,
+    };
+
 private:
     Ui::MainWindow *ui;
+
+    double ConvertUnits(Units convertFrom, Units convertTo, double value, Ingredient ingredient) const;
+    double CalculateVitalWheatGlutenPercentage(double tpp, double fpp, double vwgpp) const;
+    double CalculateAllPurposeFlourPercentage(double vwg_Percentage, double tpp, double fpp, double vwgpp) const;
+
+
+private slots:
+    void Calculate();
+
 };
 #endif // MAINWINDOW_H
