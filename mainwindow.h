8944720ac7d206fe9fbc9a3184248071fc790f0c
Donald Rudquist <donaldrudquist@gmail.com>
2020-11-21 15:44:37 -0600

    Removed enums

--- mainwindow.h
+++ mainwindow.h
@@ -17,24 +17,10 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    enum class Units{
-        eCUPS,
-        eTBSP,
-        eTSP,
-        eGRAMS,
-        eOUNCES,
-    };
-
-    enum class Ingredient{
-        eBREAD_FLOUR,
-        eALL_PURPOSE_FLOUR,
-        eVITAL_WHEAT_GLUTEN,
-    };
-
 private:
+
     Ui::MainWindow *ui;
 
-    double ConvertUnits(Units convertFrom, Units convertTo, double value, Ingredient ingredient) const;
     double CalculateVitalWheatGlutenPercentage(double tpp, double fpp, double vwgpp) const;
     double CalculateAllPurposeFlourPercentage(double vwg_Percentage, double tpp, double fpp, double vwgpp) const;
 
